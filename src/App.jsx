e85b6a2258ed93ba901185c2665df229461ff92f
Carlitos <basilecarlos12@gmail.com>
2023-08-21 11:41:58 +0200

    Navbar Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { useState } from 'react'
-import logo from './favicon.io'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from './sections'
+import  Nav  from './components/nav'
+const App = () => {
+  
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello World!
-      </h1>
-    </>
-  )
+    <main className="relative">
+     <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+     <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+  );
+  
 }
 
 export default App
