184bdc6599828fdb8b8804e9a8260febb9836446
Carlitos <basilecarlos12@gmail.com>
2023-08-21 19:40:39 +0200

    Services Section Completed

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import Button from "./Button";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
+import ServiceCard from "./ServiceCard"
 
 
 export {
     Button,
     Nav,
     ShoeCard,
+    ServiceCard
 }
