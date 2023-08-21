45fcaf8ff22d6440ac755bbdab033a78b08d30b6
Carlitos <basilecarlos12@gmail.com>
2023-08-21 17:14:10 +0200

    Hero completed

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({label, iconURL}) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-3 border font-montsserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        {label}
+        <img src={iconURL} alt="arrow right icon"  className='ml-2 rounded-full w-5 h-5'/>
+    </button>
+  )
+}
+
+export default Button
