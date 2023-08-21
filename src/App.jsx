78c6e052d5ea7d01a27106795b7f37b94e283031
Carlitos <basilecarlos12@gmail.com>
2023-08-21 17:31:08 +0200

    Shoe Cards Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,30 @@ import  Nav  from './components/nav'
 const App = () => {
   
   return (
-    <main className="relative">
-     <Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">
-     <Hero />
+    <main className='relative'>
+    <Nav />
+    <section className='xl:padding-l wide:padding-r padding-b'>
+      <Hero />
     </section>
-    <section className="padding">
+    <section className='padding'>
       <PopularProducts />
     </section>
-    <section className="padding">
+    <section className='padding'>
       <SuperQuality />
     </section>
-    <section className="padding">
+    <section className='padding-x py-10'>
       <Services />
     </section>
-    <section className="padding">
+    <section className='padding'>
       <SpecialOffer />
     </section>
-    <section className="bg-pale-blue padding">
+    <section className='bg-pale-blue padding'>
       <CustomerReviews />
     </section>
-    <section className="padding-x sm:py-32 py-16 w-full">
+    <section className='padding-x sm:py-32 py-16 w-full'>
       <Subscribe />
     </section>
-    <section className="padding bg-black padding-x padding-t pb-8">
+    <section className=' bg-black padding-x padding-t pb-8'>
       <Footer />
     </section>
   </main>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import Button from '../components/Button'
-import ShoeCard from '../components/ShoeCard'
-import { arrowRight } from '../assets/icons'
-import { shoes, statistics } from '../constants'
-import { bigShoe1 } from '../assets/images'
+import { useState } from "react";
+import { shoes, statistics } from "../constants";
+import { Button, ShoeCard } from "../components";
+import { bigShoe1 } from "../assets/images";
+import { arrowRight } from "../assets/icons";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id='home'
@@ -41,18 +42,31 @@ const Hero = () => {
           ))}
         </div>
       </div>
-      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-priamry bg-hero bg-cover bg-center'>
-        <img src={bigShoe1} alt='shoe collection' width={610} height={500} className='object-contain relative z-10' />
-        <div>
-            {shoes.map((shoe, index) => (
-              <div key={shoe}>
-                <ShoeCard shoe={shoe} index={index} />
-              </div> 
-            ))}
+
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+        <img
+          src={bigShoeImg}
+          alt='shoe colletion'
+          width={610}
+          height={502}
+          className='object-contain relative z-10'
+        />
+
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+          {shoes.map((image, index) => (
+            <div key={index}>
+              <ShoeCard
+                index={index}
+                imgURL={image}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
